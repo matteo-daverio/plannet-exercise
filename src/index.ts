086171ccbeffaf418740e25d67907ce969f83c44
Matteo Daverio <matteo.daverio@reti.it>
2021-06-21 19:28:56 +0200

    Esercizio 3

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,20 @@ import $ from "jquery";
 import { Film } from "./film";
 
 const films = new Array<Film>();
+const newFilms = new Array<Film>();
 
 function populateArray() {
     films.push(new Film("Il Signore degli Anelli - Il Ritorno del Re", 400, "Fantasy"));
     films.push(new Film("Avengers: Infinity War", 160, "Action"));
     films.push(new Film("It - Il Pagliaccio Assassino", 113, "Horror"));
+
+    newFilms.push(new Film("Tenet", 150, "Action"));
+    newFilms.push(new Film("Wonder Woman 1984", 160, "Action"));
 }
 
-function displayFilms() {
-    var html = `
-    <table id='summaryOfResults' border='1'>
+function getTable(films: Film[]) : string {
+    let html = `
+    <table id='films' border='1'>
         <thead id="body">
             <tr>
                 <th>Nome</th>
@@ -21,18 +25,21 @@ function displayFilms() {
         </thead>
         <tbody>`;
 
-    films
-        .filter(film => film.duration > 120)
-        .forEach(film => {
-            html += "<tr>";
-            Object.keys(film).forEach(prop => {
-                if (prop in film)
-                    html += '<td>' + (prop === "genre" ? film[prop].toString().substr(0, 3) : (<any>film)[prop].toString()) + '</td>';
-            });
-            html += "</tr>";
-        });
+    html += films.map(film => `<tr><td>${film.name}</td><td>${film.duration}</td><td>${film.genre.substr(0, 3)}</td></tr>`);
 
     html += "</tbody></table>";
+
+    return html;
+}
+
+function displayFilms() {
+    let html = getTable(films);
+        
+    html += "<br />" + getTable(newFilms);
+
+    let tmpFilms = [...films, ...newFilms];
+
+    html += "<br />" + getTable(tmpFilms);
 
     $("#content").append(html);
 }
